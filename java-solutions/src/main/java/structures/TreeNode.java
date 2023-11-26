1a207d5ffb0a4e9b1b16a353882305d47084d5a9
flight <mwxj@mail.ru>
2023-11-26 12:49:49 +0300

    adding a python repository, refactoring

--- java-solutions/src/main/java/structures/TreeNode.java
+++ java-solutions/src/main/java/structures/TreeNode.java
@@ -13,7 +13,7 @@ public class TreeNode {
         this.val = val;
     }
 
-    TreeNode(int val, TreeNode left, TreeNode right) {
+    public TreeNode(int val, TreeNode left, TreeNode right) {
         this.val = val;
         this.left = left;
         this.right = right;
